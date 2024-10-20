fc84a6b39d8985e4ba753de5692603e8eb3eb1f6
sjeevankumar <sjeevankumar89@gmail.com>
2024-10-20 12:33:14 +0530

    feat:add layout

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import React from "react"
+import {
+  Cryptocurrencies,
+  CryptoDetails,
+  Exchanges,
+  Homepage,
+  Navbar,
+  News,
+} from "./components"
+import "./App.css"
+import {
+  createBrowserRouter,
+  Link,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom"
+import { Space, Typography } from "antd"
+
+const App = () => {
+  const Layout = () => {
+    return (
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Outlet />
+          <div className="footer">
+            <Typography.Title level={5} style={{ color: "white" }}>
+              Cryptoverse <br />
+              All rights deserved
+            </Typography.Title>
+            <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Homepage />,
+        },
+        {
+          path: "/exchanges",
+          element: <Exchanges />,
+        },
+        {
+          path: "/cryptocurrencies",
+          element: <Cryptocurrencies />,
+        },
+        {
+          path: "/crypto/:coinId",
+          element: <CryptoDetails />,
+        },
+        {
+          path: "/news",
+          element: <News />,
+        },
+      ],
+    },
+  ])
+  return <RouterProvider router={router} />
+}
+
+export default App
---
+++ src/components/Exchanges.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Exchanges = () => {
+  return <div>Exchnages</div>
+}
+
+export default Exchanges
